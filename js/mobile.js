95cde919aa7eb27f132d8bd669a13d12eeb73564
Diabolike667 <Quedevillekeviin@outlook.fr>
2022-06-02 16:54:31 +0200

    modif commit

---
+++ js/mobile.js
@@ -0,0 +1,36 @@
+// tableau qui contient les liens de la barre NAV /
+lienMenu = document.querySelectorAll('nav ul li');
+// / tableau qui contient les Ã©crans de ma page 
+ecranAffiche = document.querySelectorAll('.ecran');
+for (let i = 0; i < lienMenu.length; i++) {
+    lienMenu[i].addEventListener('click',
+        function () { positionnerEcran(i); });
+    lienMenu[i].addEventListener('mouseover',
+        function () { changerCurseur(i); });
+}
+// Fonctions
+function positionnerEcran(numEcran) {
+    ecranAffiche[numEcran].scrollIntoView(
+        {
+            block: 'end',
+            inline: 'start',
+            behavior: 'smooth'
+        }
+    );
+}
+function changerCurseur(numLien) {
+    lienMenu[numLien].style.cursor = 'pointer';
+}
+
+
+if (window.matchMedia("(max-width: 858px)").matches) {
+    var lesclasseasupprimer = document.getElementsByClassName('hover-bg');
+    Array.from(lesclasseasupprimer).forEach((laclasseasupprimer, index) => {
+        laclasseasupprimer.classList.remove('hover-bg');
+    }
+    );
+} else {
+    /* the view port is max 858 pixels wide */
+    console.log("Grand ecran");
+}
+
